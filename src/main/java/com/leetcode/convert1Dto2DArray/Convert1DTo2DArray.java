55557659bf02d578e316ebb802a21e1830b80c95
sinh77 <amiteshsinha88@gmail.com>
2024-09-03 18:56:32 +0530

    fixing code

--- src/main/java/com/leetcode/convert1Dto2DArray/Convert1DTo2DArray.java
+++ src/main/java/com/leetcode/convert1Dto2DArray/Convert1DTo2DArray.java
@@ -4,5 +4,16 @@ public class Convert1DTo2DArray extends AbstractParent {
 
     @Override
     public int[][] construct2DArray(int[] original, int m, int n) {
+        int[][] retArr = new int[m][n];
+        int i = 0, j = 0;
+        for (int k = 0; k < original.length; k++) {
+            retArr[i][j] = original[k];
+            j++;
+            if (j == n) {
+                i++;
+                j = 0;
+            }
+        }
+        return retArr;
     }
 }
