c5f0fdd067b41016f4da2a90a17b665d6a4d183c
sinh77 <amiteshsinha88@gmail.com>
2024-09-12 12:18:05 +0530

    fix for ConvertSortedLinkedListToBST

--- src/main/java/com/leetcode/binarySearchTree/convertSortedListToBST/AbstractParent.java
+++ src/main/java/com/leetcode/binarySearchTree/convertSortedListToBST/AbstractParent.java
@@ -2,8 +2,12 @@ package com.leetcode.binarySearchTree.convertSortedListToBST;
 
 import com.amitesh.core.baseclass.ListNode;
 import com.amitesh.core.baseclass.TreeNode.TreeNode;
+import com.leetcode.binaryTree.sameTree.SameTree;
 import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
+import static com.amitesh.core.baseclass.ListNode.createList;
+import static com.amitesh.core.baseclass.TreeNode.TreeNode.constructTreeFromArray;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.assertFalse;
@@ -19,14 +23,18 @@ import static org.testng.Assert.assertFalse;
 public abstract class AbstractParent {
 
     public abstract TreeNode sortedListToBST(ListNode head);
+    SameTree sameTree = new SameTree();
 
     @Test
     public void should_give_correct_output1() {
-
+        assertTrue(sameTree.isSameTree(sortedListToBST(createList(new int[]{-10,-3,0,5,9})),
+                constructTreeFromArray(new Integer[]{0,-10,5,null,-3,null,9})));
     }
 
     @Test
     public void should_give_correct_output2() {
+        assertTrue(sameTree.isSameTree(sortedListToBST(createList(new int[]{})),
+                constructTreeFromArray(new Integer[]{})));
     }
 
     @Test
--- src/main/java/com/leetcode/binarySearchTree/convertSortedListToBST/ConvertSortedLinkedListToBST.java
+++ src/main/java/com/leetcode/binarySearchTree/convertSortedListToBST/ConvertSortedLinkedListToBST.java
@@ -3,8 +3,10 @@ package com.leetcode.binarySearchTree.convertSortedListToBST;
 import com.amitesh.core.baseclass.ListNode;
 import com.amitesh.core.baseclass.TreeNode.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConvertSortedLinkedListToBST extends AbstractParent {
-    //todo fix it
     @Override
     public TreeNode sortedListToBST(ListNode head) {
         // Step 1 - go till the mid - have 1 pointer prior to the mid
@@ -12,6 +14,23 @@ public class ConvertSortedLinkedListToBST extends AbstractParent {
         // Step 3- create tree
 
         // or get size of the list - copy data to array. and then from array create tree
-        return null;
+        List<Integer> sortedList = new ArrayList<>();
+        ListNode temp = head;
+        while (temp != null) {
+            sortedList.add(temp.val);
+            temp = temp.next;
+        }
+        return convertToBST(sortedList, 0, sortedList.size() - 1);
+    }
+
+    private TreeNode convertToBST(List<Integer> sortedList, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right)/2;
+        TreeNode root = new TreeNode(sortedList.get(mid));
+        root.left = convertToBST(sortedList, left, mid - 1);
+        root.right = convertToBST(sortedList, mid + 1, right);
+        return root;
     }
 }
