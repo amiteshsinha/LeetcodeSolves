eb32f0e28a1da27311799cfc87c4d95a697f43e9
sinh77 <amiteshsinha88@gmail.com>
2024-09-19 12:58:52 +0530

    problem for the day

---
+++ src/main/java/com/leetcode/diffWaysAddParantheses/AbstractParent.java
@@ -0,0 +1,35 @@
+package com.leetcode.diffWaysAddParantheses;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
+
+/**
+ * DiffWaysAddParantheses
+ * 19/09/24
+ * Thursday
+ *
+ * @author Amitesh Sinha
+ **/
+
+public abstract class AbstractParent {
+
+    public abstract List<Integer> diffWaysToCompute(String expression);
+
+    @Test
+    public void should_give_correct_output1() {
+
+    }
+
+    @Test
+    public void should_give_correct_output2() {
+    }
+
+    @Test
+    public void should_give_correct_output3() {
+    }
+}
---
+++ src/main/java/com/leetcode/diffWaysAddParantheses/DiffWaysAddParantheses.java
@@ -0,0 +1,11 @@
+package com.leetcode.diffWaysAddParantheses;
+
+import java.util.List;
+
+public class DiffWaysAddParantheses extends AbstractParent {
+
+    @Override
+    public List<Integer> diffWaysToCompute(String expression) {
+        return null;
+    }
+}
